65db7e760b2069193a4b252ad21fe2f17cfcfa60
j-yunjeong <jungy_jung@naver.com>
2022-02-10 13:56:51 +0900

    for,range

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 )
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func superAdd(numbers ...int) int {
+	for index := range numbers {
+		fmt.Println(numbers[index])
+	}
+	return 1
+}
+func main() {
+	superAdd(1, 2, 3, 4, 5, 6)
 }
 
-func main() {
-	repeatMe("nico", "lynn", "dal", "marl", "flynn")
-}
+/*
+	main package만 compile 가능
+	import를 이용해 package를 포함
+	return 값으로 여러 타입 가능
+	defer을 이용해 func 종료 후 명령 지정 가능
+	loop는 오로지 'for'로 표현 가능 (fuc에서 쓰는 게 가장 best)
+	like Python -> range 변수 형태로 사용
+*/
